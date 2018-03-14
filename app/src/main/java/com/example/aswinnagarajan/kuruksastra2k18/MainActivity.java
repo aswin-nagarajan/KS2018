8270daf723f79afe448eb34780cff610c018cc0d
Aswin <aswin97.nagarajan@gmail.com>
2018-03-14 12:09:06 +0530

    Home Screen on click added

--- app/src/main/java/com/example/aswinnagarajan/kuruksastra2k18/MainActivity.java
+++ app/src/main/java/com/example/aswinnagarajan/kuruksastra2k18/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.aswinnagarajan.kuruksastra2k18;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.aswinnagarajan.kuruksastra2k18.FlipPager.HomeEvent;
+import com.example.aswinnagarajan.kuruksastra2k18.FlipPager.HomeEventAdapter;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetView;
+import com.yalantis.flipviewpager.utils.FlipSettings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ListView friends = (ListView) findViewById(R.id.friends);
+
+        FlipSettings settings = new FlipSettings.Builder().defaultPage(1).build();
+
+        friends.setAdapter(new HomeEventAdapter(this, getHomeEvents(), settings));
+
+        friends.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HomeEvent f = (HomeEvent) friends.getAdapter().getItem(position);
+
+                Toast.makeText(MainActivity.this, f.getNickname(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TapTargetView.showFor(this,                 // `this` is an Activity
+                TapTarget.forView(findViewById(R.id.title), "Welcome to KS 2018", "Swipe right on the images to show description")
+                        // All options below are optional
+                        .outerCircleColor(R.color.saffron)      // Specify a color for the outer circle
+                        .outerCircleAlpha(0.96f)            // Specify the alpha amount for the outer circle
+                        .targetCircleColor(R.color.colorPrimary)   // Specify a color for the target circle
+                        .titleTextSize(30)                  // Specify the size (in sp) of the title text
+                        .titleTextColor(R.color.colorPrimary)      // Specify the color of the title text
+                        .descriptionTextSize(20)            // Specify the size (in sp) of the description text
+                        .descriptionTextColor(R.color.pink)  // Specify the color of the description text
+                        .textColor(R.color.green)            // Specify a color for both the title and description text
+                        .textTypeface(Typeface.SANS_SERIF)  // Specify a typeface for the text
+                        .dimColor(R.color.orange)            // If set, will dim behind the view with 30% opacity of the given color
+                        .drawShadow(true)                   // Whether to draw a drop shadow or not
+                        .cancelable(false)                  // Whether tapping outside the outer circle dismisses the view
+                        .tintTarget(true)                   // Whether to tint the target view's color
+                        .transparentTarget(false)           // Specify whether the target is transparent (displays the content underneath)
+//                        .icon(Drawable)                     // Specify a custom drawable to draw as the target
+                        .targetRadius(100),                  // Specify the target radius (in dp)
+                new TapTargetView.Listener() {          // The listener can listen for regular clicks, long clicks or cancels
+                    @Override
+                    public void onTargetClick(TapTargetView view) {
+                        super.onTargetClick(view);
+                    }
+                });
+
+    }
+
+    public List<HomeEvent> getHomeEvents(){
+        List<HomeEvent> friends = new ArrayList<HomeEvent>();
+        friends.add(new HomeEvent(R.drawable.anastasia, "EVENTS", R.color.sienna, "Sport", "Literature", "Music", "Art", "Technology"));
+        friends.add(new HomeEvent(R.drawable.anfer, "SCHEDULE", R.color.purple, "Android", "UI/Developer", "Soccer", "Profile", "Company"));
+        friends.add(new HomeEvent(R.drawable.irene, "PRO-SHOWS", R.color.saffron, "Travelling", "Flights", "Books", "Painting", "Design"));
+        friends.add(new HomeEvent(R.drawable.kate, "KATE", R.color.green, "Sales", "Pets", "Skiing", "Hairstyles", "Сoffee"));
+        friends.add(new HomeEvent(R.drawable.paul, "PAUL", R.color.pink, "Android", "Development", "Design", "Wearables", "Pets"));
+        friends.add(new HomeEvent(R.drawable.daria, "DARIA", R.color.orange, "Design", "Fitness", "Healthcare", "UI/UX", "Chatting"));
+        friends.add(new HomeEvent(R.drawable.kirill, "KIRILL", R.color.saffron, "Development", "Android", "Healthcare", "Sport", "Rock Music"));
+        friends.add(new HomeEvent(R.drawable.julia, "JULIA", R.color.green, "Cinema", "Music", "Tatoo", "Animals", "Management"));
+        return friends;
     }
 }
